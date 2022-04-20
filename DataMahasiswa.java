619a13582863db0f63a5b5b560c9bfcdf8b15e4b
akbarverari1 <79489734+akbarverari1@users.noreply.github.com>
2022-04-20 23:47:11 +0700

    class baru untuk data diri
    class baru untuk data diri

---
+++ DataMahasiswa.java
@@ -0,0 +1,36 @@
+package lib;
+
+import java.util.Date;
+
+public class DataMahasiswa {
+
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+	
+	public String getfirstName() {
+		return firstName;
+	}
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getlasttName() {
+		return lastName;
+	}
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getidNumber() {
+		return idNumber;
+	}
+	public void setidNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+	public String getaddress() {
+		return address;
+	}
+	public void setaddress(String address) {
+		this.address = address;
+	}
+}
